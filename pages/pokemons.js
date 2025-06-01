6565c43d33148b4096317be94e08d692eece20d6
Frolum189 <97343278+Frolum189@users.noreply.github.com>
2025-06-01 16:38:45 +0300

    Add files via upload

--- pages/pokemons.js
+++ pages/pokemons.js
@@ -4,8 +4,9 @@ export async function pokemonsPage(app) {
     app.innerHTML = `<h2>Loading pokemons...</h2>`
 
     const data = await getPokemonList(10);
-    const cards = data.results.map((poke, index) => `
+    const cards = data[0].results.map((poke, index) => `
         <div class="card">
+            <img src="${data[1][index]}" alt="${poke.name}"/>
             <a href="#/pokemons/${index + 1}">${poke.name}</a>
         </div>
     `).join('');
@@ -15,3 +16,50 @@ export async function pokemonsPage(app) {
         <div class="grid">${cards}</div>
     `;
 }
+
+async function findpokemon() {
+    let inptvalue = document.getElementById("finder").value.toLowerCase();
+    let data = await getPokemonList(10);
+    let pokemon = data.results.find(poke => poke.name.toLowerCase() === inptvalue);
+
+    if (pokemon) {
+        let index = data.results.indexOf(pokemon) + 1;
+        window.location.hash = `#/pokemons/${index}`;
+    } else {
+        alert('Pokemon not found');
+    }
+}
+
+async function filterbypower() {
+    let data = await getPokemonList(10);
+    let strongest = data.reduce((max, current) => {
+        let currentAttack = current.stats.find(stat => stat.name === 'attack')?.base || 0;
+        let maxAttack = max.stats.find(stat => stat.name === 'attack')?.base || 0;
+        return currentAttack > maxAttack ? current : max;
+    });
+    window.location.hash = `#/pokemons/${strongest.id}`;
+}
+
+async function filterbyspeed() {
+    let data = await getPokemonList(10);
+    let fastest = data.reduce((max, current) => {
+        let currentSpeed = current.stats.find(stat => stat.name === 'speed')?.base || 0;
+        let maxSpeed = max.stats.find(stat => stat.name === 'speed')?.base || 0;
+        return currentSpeed > maxSpeed ? current : max;
+    });
+    window.location.hash = `#/pokemons/${fastest.id}`;
+}
+
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropdown-btn')) {
+        let dropdown = document.getElementById('filterDropdown');
+        if (dropdown.classList.contains('show')) {
+            dropdown.classList.remove('show');
+        }
+    }
+}
+
+document.getElementById('findbutton').addEventListener('click', findpokemon);
+document.getElementById('filterbypower').addEventListener('click',filterbypower);
+document.getElementById('filterbyspeed').addEventListener('click', filterbyspeed);
--- services/pokeapi.js
+++ services/pokeapi.js
@@ -1,9 +1,30 @@
 const BASE_URL = 'https://pokeapi.co/api/v2/';
 
 export async function getPokemonList(limit = 20, offset = 0) {
-    const result = await fetch(`${BASE_URL}/pokemon?limit=${limit}&offset=${offset}`);
-    return await result.json();
+    const result = await fetch(`${BASE_URL}pokemon?limit=${limit}&offset=${offset}`);
+    const data = await result.json();
+
+    let detailedList = await Promise.all(
+        data.results.map(async (pokemon) => {
+            let detailsRes = await fetch(pokemon.url);
+            let details = await detailsRes.json();
+
+            return {
+                name: details.name,
+                id: details.id,
+                stats: details.stats.map(stat => ({
+                    name: stat.stat.name,
+                    base: stat.base_stat
+                })),
+                sprite: details.sprites.front_default
+            };
+        })
+    );
+
+    return detailedList;
 }
+
+
 
 export async function getPokemonById(id) {
     const result = await fetch(`${BASE_URL}/pokemon/${id}`);
